9316ba5c58a60a02f3d851ed35c2134b9258e7cc
Michael Yanovich <michael@yanovich.net>
2012-09-19 21:10:49 +0000

    fixed ASCII/unicode problems

--- modules/rand.py
+++ modules/rand.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 rand.py - Rand Module
-Copyright 2010, Michael Yanovich, yanovich.net
+Copyright 2010-12, Michael Yanovich, yanovich.net
 Licensed under the Eiffel Forum License 2.
 
 More info:
@@ -14,7 +14,7 @@ import re
 
 def rand(jenni, input):
     """.rand <arg1> <arg2> - Generates a random integer between <arg1> and <arg2>."""
-    if input.group(2) == " " or input.group(2) == "" or str(input.group(2)) == None or str(input.group(2)) == "" or input.group(2) == None:
+    if input.group(2) == " " or not input.group(2):
         jenni.say("I'm sorry, " + str(input.nick) + ", but you must enter at least one number.")
     else:
         random.seed()
@@ -22,26 +22,40 @@ def rand(jenni, input):
         li_integers_str = li_integers.split()
         if len(li_integers_str) == 1:
             li_integers_str = re.sub(r'\D', '', str(li_integers_str))
-            if int(li_integers_str[0]) <= 1:
-                a = li_integers_str
-                a = int(a)
-                randinte = random.randint(a, 0)
+            if len(li_integers_str) > 0:
+                if int(li_integers_str[0]) <= 1:
+                    a = li_integers_str
+                    a = int(a)
+                    if a < 0:
+                        randinte = random.randint(a, 0)
+                    if a > 0:
+                        randinte = random.randint(0, a)
+                else:
+                    a = li_integers_str
+                    a = int(a)
+                    randinte = random.randint(0, a)
+                jenni.say(str(input.nick) + ": your random integer is: " + str(randinte))
             else:
-                a = li_integers_str
-                a = int(a)
-                randinte = random.randint(0, a)
-            jenni.say(str(input.nick) + ": your random integer is: " + str(randinte))
+                jenni.reply("lolwut")
         else:
-            a,b = li_integers.split()
-            a = re.sub(r'\D', '', str(a))
-            b = re.sub(r'\D', '', str(b))
-            a = int(a)
-            b = int(b)
-            if a <= b:
-                randinte = random.randint(a, b)
+            ln = li_integers.split()
+            if len(ln) == 2:
+                a, b = ln
+                a = re.sub(r'\D', u'', a)
+                b = re.sub(r'\D', u'', b)
+                if not a:
+                    a = 0
+                if not b:
+                    b = 0
+                a = int(a)
+                b = int(b)
+                if a <= b:
+                    randinte = random.randint(a, b)
+                else:
+                    randinte = random.randint(b, a)
+                jenni.say(str(input.nick) + ": your random integer is: " + str(randinte))
             else:
-                randinte = random.randint(b, a)
-            jenni.say(str(input.nick) + ": your random integer is: " + str(randinte))
+                jenni.reply("I'm not sure what you want me to do!")
 
 rand.commands = ['rand']
 rand.priority = 'medium'
