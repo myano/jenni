a4533a1b6ed8f6878ff9688bf7b9d290a7f0adda
Michael Yanovich <michael@yanovich.net>
2013-02-13 04:55:34 +0000

    added in a imdb/movie module from Willie

---
+++ modules/movie.py
@@ -0,0 +1,52 @@
+# -*- coding: utf8 -*-
+"""
+imdb.py - jenni Movie Information Module
+Copyright 2012, Elad Alfassa, <elad@fedoraproject.org>
+Licensed under the Eiffel Forum License 2.
+
+This module relies on imdbapi.com
+
+More info:
+ * jenni: https://github.com/myano/jenni/
+ * Phenny: http://inamidst.com/phenny/
+"""
+
+import web
+import urllib2
+
+
+def movie(jenni, input):
+    """.imdb movie/show title -- displays information about a production"""
+
+    if not input.group(2):
+        return
+    word = input.group(2).rstrip()
+    word = word.replace(" ", "+")
+    uri = "http://www.imdbapi.com/?t=" + word
+
+    uri = uri.encode('utf-8')
+    page = web.get(uri)
+    data = web.json(page)
+
+    if data['Response'] == 'False':
+        if 'Error' in data:
+            message = '[MOVIE] %s' % data['Error']
+        else:
+            jenni.debug('movie',
+                        'Got an error from the imdb api,\
+                                search phrase was %s' %
+                        word, 'warning')
+            jenni.debug('movie', str(data), 'warning')
+            message = '[MOVIE] Got an error from imdbapi'
+    else:
+        message = '[MOVIE] Title: ' + data['Title'] + \
+                  ' | Year: ' + data['Year'] + \
+                  ' | Rating: ' + data['imdbRating'] + \
+                  ' | Genre: ' + data['Genre'] + \
+                  ' | IMDB Link: http://imdb.com/title/' + data['imdbID']
+    jenni.say(message)
+movie.commands = ['movie', 'imdb']
+movie.example = '.movie Movie Title'
+
+if __name__ == '__main__':
+    print __doc__.strip()
