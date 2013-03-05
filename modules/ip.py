e612c9e87aee5762c62d0a800c2b2582ea27a2b1
Michael Yanovich <michael@yanovich.net>
2013-03-05 05:45:37 +0000

    Improved ip/host lookup module
    This change significantly fixes and changes the output and handling of
    the .ip and .host. It currently does not account for IPv6 geolocation
    lookup. This will be added in the future.

--- modules/ip.py
+++ modules/ip.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # coding=utf-8
 """
 ip.py - jenni IP Lookup Module
@@ -6,54 +7,75 @@ Copyright 2011, Dimitri Molenaars (TyRope.nl)
 Licensed under the Eiffel Forum License 2.
 
 More info:
-  * Willie: http://willie.dftba.net
-  * jenni: https://github.com/myano/jenni/
-  * Phenny: http://inamidst.com/phenny/
+ * Willie: http://willie.dftba.net
+ * jenni: https://github.com/myano/jenni/
+ * Phenny: http://inamidst.com/phenny/
 
-This module has been improted from Willie.
+This module has been imported from Willie.
 """
 
 import re
+import socket
 import web
 
-def ip(jenni, input):
-    """IP Lookup tool"""
-    if not input.group(2):
-        return jenni.reply("No search term.")
-    query = input.group(2).encode('utf-8')
-    uri = 'http://www.rscript.org/lookup.php?type=ipdns&ip='
-    answer = web.get(uri + web.quote(query.replace('+', '%2B')))
-    if answer:
-        invalid = re.search("(?:INVALID: )([\S ]*)", answer)
-        if invalid:
-            response = "[IP/Host Lookup] "+invalid.group(1)
-        else:
-            #parse stuffs.
-            host = re.search("(?:Hostname:[ ]?)([\S ]*)", answer)
-            isp = re.search("(?:ISP:[ ]?)([\S ]*)", answer)
-            org = re.search("(?:Organization:[ ]?)([\S ]*)(?:Services:)", answer)
-            typ = re.search("(?:Type:[ ]?)([\S ]*)", answer)
-            assign = re.search("(?:Assignment:[ ]?)([\S ]*)", answer)
-            city = re.search("(?:City:[ ]?)([\S ]*)", answer)
-            state = re.search("(?:State/Region:[ ]?)([\S ]*)", answer)
-            country = re.search("(?:Country:[ ]?)([\S ]*)(?:  )", answer)
+base = "http://whatismyipaddress.com/ip/"
+r_tag = re.compile(r'<(?!!)[^>]+>')
 
-            if not host or not isp or not org or not typ or not assign or not city or not state or not country:
-                response = "[IP/Host Lookup] Something went wrong, please try again."
-            else:
-                response = "[IP/Host Lookup] Hostname: "+host.group(1)
-                response += " | ISP: "+isp.group(1)
-                response += " | Organization: "+org.group(1)
-                response += " | Type: "+typ.group(1)
-                response += " | Assignment: "+assign.group(1)
-                response += " | Location: "+city.group(1)
-                response += ", "+state.group(1)
-                response += ", "+country.group(1)+"."
-        jenni.say(response)
+def grab_info(ip):
+    rdict = dict()
+    answer = web.get(base + ip)
+    if answer:
+        results = re.compile("th>(\S+?):<.*?<td>(.*?)</td>").findall(answer)
+        for x in results:
+            rdict[x[0]] = r_tag.sub("", x[1]).strip()
+    return rdict
+
+def gen_response(rdict, ip=False):
+    response = str()
+    if not ip:
+        response = "Hostname: " + rdict['Hostname']
+    response += " | ISP: " + rdict['ISP']
+    response += " | Organization: " + rdict['Organization']
+    response += " | Type: " + rdict['Type']
+    response += " | Assignment: " + rdict['Assignment']
+    response += " | Location: " + rdict['City']
+    response += ", " + rdict['State/Region']
+    response += ", " + rdict['Country'] + "."
+    return response
+
+
+def getipv4(q):
+    resp = str()
+    for x in q:
+        ip = x[-1][0]
+        try:
+            resp += gen_response(grab_info(ip), True)
+        except:
+            continue
+        if resp:
+            return resp, ip
+
+
+def ip_lookup(jenni, input):
+    hip = input.group(2)
+    if not hip:
+        return jenni.reply("No search term!")
+    query = hip.encode('utf-8')
+    answer = grab_info(query)
+    response = "[IP/Host Lookup] "
+    if answer:
+        response += gen_response(answer)
     else:
-        jenni.reply('Sorry, no result.')
-ip.commands = ['iplookup','ip']
-ip.example = '.iplookup 8.8.8.8'
+        q = socket.getaddrinfo(query, 80)
+        try:
+            resp, ip = getipv4(q)
+            response += "IP Address: " + str(ip)
+            response += resp
+        except:
+            response += "No results currently found for: %s" % (hip)
+    jenni.reply(response)
+ip_lookup.commands = ['ip', 'iplookup', 'host']
+ip_lookup.example = ".iplookup 8.8.8.8"
 
 if __name__ == '__main__':
     print __doc__.strip()
