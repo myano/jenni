c4d5c28c609d05233163d548863ff42851b0f1cf
Michael Yanovich <michael@yanovich.net>
2013-03-22 07:02:59 +0000

    Added error handling

--- modules/quote.py
+++ modules/quote.py
@@ -14,14 +14,14 @@ from modules import unicode as uc
 
 
 def addquote(jenni, input):
-    """.addquote <nick> something they said here -- adds the quote to the quote database."""
+    '''.addquote <nick> something they said here -- adds the quote to the quote database.'''
     text = input.group(2)
-    fn = open("quotes.txt", "a")
+    fn = open('quotes.txt', 'a')
     output = uc.encode(text)
     fn.write(output)
-    fn.write("\n")
+    fn.write('\n')
     fn.close()
-    jenni.reply("Quote added.")
+    jenni.reply('Quote added.')
 addquote.commands = ['addquote']
 addquote.priority = 'low'
 addquote.example = '.addquote'
@@ -29,9 +29,13 @@ addquote.rate = 30
 
 
 def retrievequote(jenni, input):
-    """.quote <number> -- displays a given quote"""
+    '''.quote <number> -- displays a given quote'''
     text = input.group(2)
-    fn = open("quotes.txt", "r")
+    try:
+        fn = open('quotes.txt', 'r')
+    except:
+        return jenni.reply('Please add a quote first.')
+
     lines = fn.readlines()
     MAX = len(lines)
     fn.close()
@@ -46,7 +50,7 @@ def retrievequote(jenni, input):
         jenni.reply("I'm not sure which quote you would like to see.")
     else:
         line = lines[number - 1]
-        jenni.reply("Quote %s of %s: " % (number, MAX) + line)
+        jenni.reply('Quote %s of %s: ' % (number, MAX) + line)
 retrievequote.commands = ['quote']
 retrievequote.priority = 'low'
 retrievequote.example = '.quote'
@@ -54,29 +58,32 @@ retrievequote.rate = 30
 
 
 def delquote(jenni, input):
-    """.rmquote <number> -- removes a given quote from the database. Can only be done by the owner of the bot."""
+    '''.rmquote <number> -- removes a given quote from the database. Can only be done by the owner of the bot.'''
     if not input.owner: return
     text = input.group(2)
     number = int()
-    fn = open("quotes.txt", "r")
+    try:
+        fn = open('quotes.txt', 'r')
+    except:
+        return jenni.reply('No quotes to delete.')
     lines = fn.readlines()
     MAX = len(lines)
     fn.close()
     try:
         number = int(text)
     except:
-        jenni.reply("Please enter the quote number you would like to delete.")
+        jenni.reply('Please enter the quote number you would like to delete.')
         return
     newlines = lines[:number-1] + lines[number:]
-    fn = open("quotes.txt", "w")
+    fn = open('quotes.txt', 'w')
     for line in newlines:
         txt = uc.encode(line)
         if txt:
             fn.write(txt)
-            if txt[-1] != "\n":
-                fn.write("\n")
+            if txt[-1] != '\n':
+                fn.write('\n')
     fn.close()
-    jenni.reply("Successfully deleted quote %s." % (number))
+    jenni.reply('Successfully deleted quote %s.' % (number))
 delquote.commands = ['rmquote', 'delquote']
 delquote.priority = 'low'
 delquote.example = '.rmquote'
