daad3d3e1884ace3fb58a84009b42421fdd4b3b1
Michael Yanovich <michael@yanovich.net>
2013-04-17 05:58:33 +0000

    changed to a different API for bitcions

--- modules/units.py
+++ modules/units.py
@@ -9,84 +9,64 @@ More info:
  * Phenny: http://inamidst.com/phenny/
 '''
 
+import datetime as dt
+import json
 import re
-import time
 import web
+
+exchange_rates = dict()
+last_check = dt.datetime.now()
+exchanges = ['mtgox', 'btc24', 'bitfloor', 'vcx', 'btce', 'rock', 'bitme',
+             'ripple', 'lybit']
 
 
 def btc_page():
     try:
-        page = web.get('http://www.ounce.me/')
+        page = web.get('http://bitcoincharts.com/t/markets.json')
     except Exception, e:
         print time.time(), btc, e
-        return False, 'Failed to reach ounce.me'
+        return False, 'Failed to reach bitcoincharts.com'
     return True, page
 
-def usd_price():
-    find_usd = re.compile('\$\d+\.\d+')
-    status, page = btc_page()
-    amounts = find_usd.findall(page)
-    if amounts:
-        amount = amounts[0][1:]
-    try:
-        amount = float(amount)
-    except:
-        amount = 0.0
-    return amount
 
 def ppnum(num):
     return re.sub("(?!\..*)(\d)(?=(\d{3})+(?!\d))", r"\1,", "%.2f" % num)
 
 
-def find_float(num):
-    try:
-        out = float(num)
-    except:
-        out = 0
-    return out
-
-
 def btc(jenni, input):
     '''.btc -- display the current prices for Bitcoins'''
-    txt = input.group(2)
-    if txt:
-        txt = txt.strip()
-        amount = usd_price()
-        txt = txt.replace(' ', '')
-        float_inc = 0
-        inc = txt.lower().replace('usd', '').replace('btc', '')
-        float_inc = find_float(inc)
-        if 'usd' in txt.lower() and 'btc' not in txt.lower():
-            response = 'BTC: '
-            response += str(ppnum(float_inc / amount))
-        elif 'btc' in txt.lower() and 'usd' not in txt.lower():
-            response = 'USD: '
-            response += str(ppnum(float_inc * amount))
-        else:
-            response = 'Defaulting to, USD: '
-            response += str(ppnum(float_inc * amount))
-        if float_inc:
-            jenni.reply(response)
-        else:
-            jenni.reply('I could not understand your input!')
-    else:
+    global exchange_rates
+    global last_check
+    now = dt.datetime.now()
+    print 'now: ', now
+    print 'last: ', last_check
+    if (not exchange_rates) or (now - last_check > dt.timedelta(minutes=15)):
+    #if now - last_check > 900:
         status, page = btc_page()
-        if not status:
-            jenni.reply(page)
-        prices = re.compile('(?i)((?:Bitcoin|Mt).*?)\|').findall(page)
-        updated = re.compile('<span class="date">(.*?)</span>').findall(page)
-        amount = usd_price()
-        response = ' | '.join(x.strip() for x in prices[:6])
-        if usd_price:
-            response += ' | Lolcat Index = $'
-            lolcat = amount * 160
-            total = ppnum(lolcat)
-            response += str(total)
-        response += ' -- Last updated at: %s EDT (-4 GMT)' % (updated[0])
-        jenni.reply(response)
+        if status:
+            json_page = json.loads(page)
+        else:
+            return jenni.reply(page)
+        ## build internal state of exchange
+        for each in json_page:
+            if each['currency'] == 'USD':
+                if 'USD' not in exchange_rates:
+                    exchange_rates['USD'] = dict()
+                exchange_rates['USD'][each['symbol'].replace('USD', '')] = each['close']
+        last_check = dt.datetime.now()
+
+    response = '1 BTC (in USD) = '
+    symbols = exchange_rates['USD'].keys()
+    symbols.sort()
+    for each in symbols:
+        if each.replace('USD', '') in exchanges:
+            response += '%s: %s | ' % (each, exchange_rates['USD'][each])
+    response += 'lolcat (mtgox) index: %s | ' % (ppnum(float(exchange_rates['USD']['mtgox']) * 160))
+    response += 'last updated at: ' + str(last_check)
+    jenni.reply(response)
 btc.commands = ['btc']
 btc.example = '.btc'
-btc.rate = 90
+btc.rate = 5
 
 
 if __name__ == '__main__':
