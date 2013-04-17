29ad92ab37998a894875bc17dff9dd50ce43cbf7
Michael Yanovich <michael@yanovich.net>
2013-04-17 06:08:26 +0000

    fixed bug with chat, to blog /ctcp and added documentation and PEP8-ified

--- modules/chat.py
+++ modules/chat.py
@@ -12,6 +12,7 @@ More info:
 
 import cleverbot
 import json
+import random
 import re
 import time
 import web
@@ -25,7 +26,8 @@ def chat(jenni, input):
     txt = input.groups()
     if len(txt) > 0:
         text = txt[1]
-        if txt[1].startswith('\x03'):
+        if txt[1].startswith('\x03') or txt[1].startswith('\x01'):
+            ## block out /ctcp
             return
     else:
         print time.time(), 'Something went wrong with chat.py'
@@ -40,11 +42,11 @@ def chat(jenni, input):
     msgi = text.strip()
     msgo = str()
     if channel.startswith('#') and txt[0]:
-        ## in a channel and prefaced with jenni's name
+        ## in a channel and prepended with jenni's name
         pm = False
         msgo = mycb.Ask(msgi)
     elif not channel.startswith('#'):
-        ## in a PM and not prefered with jenni's name
+        ## in a PM and not prepended with jenni's name
         pm = True
         if text.startswith('.'):
             return
@@ -58,10 +60,14 @@ def chat(jenni, input):
         ## anything else
         return
     if msgo:
+        rand_num = random.randint(0, 10)
+        time.sleep(3 + rand_num)
         response = re.sub('(?i)cleverbot', 'jenni', msgo)
-        response = re.sub('(?i)bot', 'human', msgo)
         if pm:
             jenni.say(response)
+            beginning = ':%s PRIVMSG %s :' % (jenni.config.nick, input.sender)
+            if hasattr(jenni.config, 'logchan_pm'):
+                jenni.msg(jenni.config.logchan_pm, beginning + response)
         else:
             jenni.reply(response)
 chat.rule = r'($nickname[:,]\s)?(.*)'
