a81e07f0974b54c8bf78c134f231cba3272a17ed
Patrick Andrew <pandrew@isilon.com>
2013-04-16 23:35:35 -0700

    Fix copyright and licensing notice

--- modules/spotify.py
+++ modules/spotify.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python
 """
 spotify.py - An api interface for spotify lookups
-Copyright 2012
-Patrick Andrew <missionsix@gmail.com>
+Copyright 2012 Patrick Andrew <missionsix@gmail.com>
+
+Licensed under the Eiffel Forum License, version 2
+
+1. Permission is hereby granted to use, copy, modify and/or
+   distribute this package, provided that:
+      * copyright notices are retained unchanged,
+      * any distribution of this package, whether modified or not,
+        includes this license text.
+2. Permission is hereby also granted to distribute binary programs
+   which depend on this package. If the binary program depends on a
+   modified version of this package, you are encouraged to publicly
+   release the modified version of this package.
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT WARRANTY. ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE AUTHORS BE LIABLE TO ANY PARTY FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THIS PACKAGE.
 """
 
 import httplib
