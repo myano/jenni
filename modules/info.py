abae4a5bb979598a6ccc312c0cd1e261a19f5023
Michael Yanovich <michael@yanovich.net>
2013-06-25 03:41:08 +0000

    changed link for documentation

--- modules/info.py
+++ modules/info.py
@@ -55,7 +55,7 @@ commands.priority = 'low'
 def help(jenni, input):
     response = (
         'Hi, I\'m a bot. Say ".commands" to me in private for a list ' +
-        'of my commands, or see http://inamidst.com/phenny/ for more ' +
+        'of my commands, or see https://github.com/myano/jenni/wiki for more ' +
         'general details. My owner is %s.'
     ) % jenni.config.owner
     jenni.reply(response)
