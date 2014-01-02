a50ec23da40f179dbaac05faa750928721354bed
kaneda <kanedasan@gmail.com>
2014-01-01 21:27:15 -0500

    Modified path to modules from home to current bot.py path

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ class Jenni(irc.Bot):
 
         filenames = []
         if not hasattr(self.config, 'enable'):
-            for fn in os.listdir(os.path.join(home, 'modules')):
+            for fn in os.listdir(os.path.join(os.getcwd(), 'modules')):
                 if fn.endswith('.py') and not fn.startswith('_'):
                     filenames.append(os.path.join(home, 'modules', fn))
         else:
