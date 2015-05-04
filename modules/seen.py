1532b857480662ea858e8447443c1d60eff96237
Michael Yanovich <michael@yanovich.net>
2015-05-04 19:52:40 +0000

    fixed bug with seen
    * if a module is wrapped with "deprecated" apparently the "excludes"
      function that blocks modules per channel in the config doesn't work.
      This module has been re-written to fix that.

--- modules/seen.py
+++ modules/seen.py
@@ -11,44 +11,35 @@ More info:
 """
 
 import time
-from tools import deprecated
 
 ## TODO: Make it save .db to disk
 
-@deprecated
-def f_seen(self, origin, match, args):
+def f_seen(jenni, input):
     """.seen <nick> - Reports when <nick> was last seen."""
-    if not match.group(2):
-        return self.msg(origin.sender, 'Please provide a nick.')
-    nick = match.group(2).lower()
-    if not hasattr(self, 'seen'):
-        return self.msg(origin.sender, '?')
-    if self.seen.has_key(nick):
-        channel, t = self.seen[nick]
-        t = time.strftime('%Y-%m-%d %H:%M:%S UTC', time.gmtime(t))
 
-        #msg = "I last saw %s at %s on %s" % (nick, t, channel)
+    if not input.group(2):
+        return jenni.say('Please provide a nick.')
+    nick = input.group(2).lower()
+
+    if not hasattr(jenni.bot, 'seen'):
+        return jenni.reply('?')
+
+    if jenni.bot.seen.has_key(nick):
+        channel, t = jenni.bot.seen[nick]
+        t = time.strftime('%Y-%m-%d %H:%M:%S UTC', time.gmtime(t))
         msg = 'I last saw %s at %s in some channel.' % (nick, t)
-        self.msg(origin.sender, str(origin.nick) + ': ' + msg)
-    else: self.msg(origin.sender, "Sorry, I haven't seen %s around." % nick)
-f_seen.rule = (['seen'], r'(\S+)')
+        jenni.say(msg)
+    else:
+        jenni.say("Sorry, I haven't seen %s around." % nick)
+f_seen.commands = ['seen']
 f_seen.rate = 15
 
-@deprecated
-def f_note(self, origin, match, args):
-    def note(self, origin, match, args):
-        if not hasattr(self.bot, 'seen'):
-            self.bot.seen = {}
-        if origin.sender.startswith('#'):
-            # if origin.sender == '#inamidst': return
-            self.seen[origin.nick.lower()] = (origin.sender, time.time())
-
-        # if not hasattr(self, 'chanspeak'):
-        #     self.chanspeak = {}
-        # if (len(args) > 2) and args[2].startswith('#'):
-        #     self.chanspeak[args[2]] = args[0]
-
-    try: note(self, origin, match, args)
+def f_note(jenni, input):
+    try:
+        if not hasattr(jenni.bot, 'seen'):
+            jenni.bot.seen = dict()
+        if input.sender.startswith('#'):
+            jenni.bot.seen[input.nick.lower()] = (input.sender, time.time())
     except Exception, e: print e
 f_note.rule = r'(.*)'
 f_note.priority = 'low'
