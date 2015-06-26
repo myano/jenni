6bdf6d44c546c37768cbb00c0055660bbe1fa8d7
Kevin Holland <kholland950@capital.edu>
2015-06-26 15:34:34 -0400

    Converted magic8Ball.py to 4 space tabs rather than tab char

--- modules/magic8Ball.py
+++ modules/magic8Ball.py
@@ -20,37 +20,37 @@ from datetime import datetime
 random.seed()
 
 def magic8Ball(jenni, input):
-	#real yes or no answers
-	answers = [	'It is certain',
-				'It is decidedly so',
-				'Without a doubt',
-				'Yes definitely',
-				'You may rely on it',
-				'As I see it, yes',
-				'Most likely',
-				'Outlook good',
-				'Yes',
-				'Signs point to yes',
-				
-				'Don\'t count on it',
-				'My reply is no',
-				'My sources say no',
-				'Outlook not so good',
-				'Very doubtful']
-	#try again answer
-	askAgainAnswers = [ 'Reply hazy try again',
-				'Ask again later',
-				'Better not tell you now',
-				'Cannot predict now',
-				'Concentrate and ask again']
-	
-	#1 in 5 chance of getting an ask again answer (randint() is inclusive)
-	if (random.randint(1,5) == 1):
-		jenni.reply(random.choice(askAgainAnswers))
-	else:  #else produce a real answer
-		hash = hashlib.sha224(input)
-		hash.update(str(datetime.now().date()))	
-		jenni.reply(answers[int(hash.hexdigest(), 16) % len(answers)])			
+    #real yes or no answers
+    answers = [    'It is certain',
+                'It is decidedly so',
+                'Without a doubt',
+                'Yes definitely',
+                'You may rely on it',
+                'As I see it, yes',
+                'Most likely',
+                'Outlook good',
+                'Yes',
+                'Signs point to yes',
+                
+                'Don\'t count on it',
+                'My reply is no',
+                'My sources say no',
+                'Outlook not so good',
+                'Very doubtful']
+    #try again answer
+    askAgainAnswers = [ 'Reply hazy try again',
+                'Ask again later',
+                'Better not tell you now',
+                'Cannot predict now',
+                'Concentrate and ask again']
+    
+    #1 in 5 chance of getting an ask again answer (randint() is inclusive)
+    if (random.randint(1,5) == 1):
+        jenni.reply(random.choice(askAgainAnswers))
+    else:  #else produce a real answer
+        hash = hashlib.sha224(input)
+        hash.update(str(datetime.now().date()))    
+        jenni.reply(answers[int(hash.hexdigest(), 16) % len(answers)])            
 
 
 magic8Ball.commands = ['magic8Ball', 'm8b']
