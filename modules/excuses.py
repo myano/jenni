34cd46bdd4a56b61cde63200921dea6a2ac84373
Michael Yanovich <michael@yanovich.net>
2015-07-08 04:48:19 +0000

    added excuses module

---
+++ modules/excuses.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""
+excuses.py - jenni Excuse Module
+Copyright 2009-2013, Michael Yanovich (yanovich.net)
+Licensed under the Eiffel Forum License 2.
+
+More info:
+ * jenni: https://github.com/myano/jenni/
+ * Phenny: http://inamidst.com/phenny/
+"""
+
+import re
+import proxy
+
+
+def excuse(jenni, input):
+    a = re.compile('<a [\s\S]+>(.*)</a>')
+    try:
+        page = proxy.get('http://programmingexcuses.com/')
+    except:
+        return jenni.say("I'm all out of excuses!")
+    results = a.findall(page)
+    if results:
+        result = results[0]
+        result = result.strip()
+        if result[-1] not in ['.', '?', '!']:
+            print 'lastchar:', str(result[-1])
+            result += '.'
+        jenni.say(result)
+    else:
+        jenni.say("I'm too lazy to find an excuse.")
+excuse.commands = ['excuse', 'excuses']
+
+
+if __name__ == '__main__':
+    print __doc__.strip()
