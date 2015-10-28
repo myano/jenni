47d22414cb42fb7bdf8f38f040181682ceb4a885
Michael Yanovich <michael@yanovich.net>
2015-10-28 03:16:51 +0000

    added doc stuffs for pun module

--- modules/pun.py
+++ modules/pun.py
@@ -11,20 +11,21 @@ More info:
 """
 
 import re
-from modules import proxy
 import web
+
 
 def puns(jenni, input):
     url = 'http://www.punoftheday.com/cgi-bin/randompun.pl'
     exp = re.compile(r'<div class="dropshadow1">\n<p>(.*?)</p>\n</div>')
-    page = proxy.get(url)
+    page = web.get(url)
 
     result = exp.search(page)
     if result:
         pun = result.groups()[0]
-        jenni.say(pun)
+        return jenni.say(pun)
     else:
-        jenni.say("I'm afraid I'm not feeling punny today!")
-
-puns.priority = 'low'
+        return jenni.say("I'm afraid I'm not feeling punny today!")
 puns.commands = ['puns', 'pun']
+
+if __name__ == '__main__':
+    print __doc__.strip()
