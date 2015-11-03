28a1473d47ef86e33d2bdc973715cfa6bac969b1
Michael Yanovich <michael@yanovich.net>
2015-11-03 15:30:39 +0000

    moved some btc code around

--- modules/units.py
+++ modules/units.py
@@ -9,12 +9,11 @@ More info:
  * Phenny: http://inamidst.com/phenny/
 '''
 
+from modules import proxy
 from modules import unicode as uc
 import datetime as dt
 import json
 import re
-import time
-import web
 
 exchange_rates = dict()
 last_check = dt.datetime.now()
@@ -23,20 +22,20 @@ exchanges = ['btce', 'rock', 'ripple', 'bitstamp', 'coinbase']
 
 def btc_page():
     try:
-        page = web.get('https://api.bitcoincharts.com/v1/markets.json')
+        page = proxy.get('https://api.bitcoincharts.com/v1/markets.json')
     except Exception, e:
-        print time.time(), e
+        print dt.datetime.now(), e
         return False, 'Failed to reach bitcoincharts.com'
     return True, page
 
+
 def btc_coinbase_page():
     try:
-        page = web.get('https://coinbase.com/api/v1/currencies/exchange_rates')
+        page = proxy.get('https://coinbase.com/api/v1/currencies/exchange_rates')
     except Exception, e:
-        print time.time(), e
+        print dt.datetimenow.now(), e
         return False, 'Failed to reach coinbase.com'
     return True, page
-
 
 
 def ppnum(num):
@@ -47,71 +46,85 @@ def btc(jenni, input):
     '''.btc -- display the current prices for Bitcoins'''
     global exchange_rates
     global last_check
+
     now = dt.datetime.now()
+
     if (not exchange_rates) or (now - last_check > dt.timedelta(minutes=15)):
         status, page = btc_page()
         json_page = dict()
+
         if status:
             try:
                 json_page = json.loads(page)
             except:
-                #return jenni.say('I could not understand the response from the server. Please try again.')
                 pass
+
         else:
             return jenni.reply(page)
+
         ## build internal state of exchange
         for each in json_page:
             if each['currency'] == 'USD':
                 if 'USD' not in exchange_rates:
                     exchange_rates['USD'] = dict()
-                exchange_rates['USD'][each['symbol'].replace(
-                    'USD', '')] = each['close']
+                exchange_rates['USD'][each['symbol'].replace('USD', '')] = each['close']
         last_check = dt.datetime.now()
 
         coinbase_status, coinbase_page = btc_coinbase_page()
+
         try:
             coinbase_json = json.loads(coinbase_page)
         except:
-            #return jenni.say('I could not understand the response from the server. Please try again.(cb)')
             pass
+
         exchange_rates['USD']['coinbase'] = ppnum(float(coinbase_json['btc_to_usd']))
 
     response = '1 BTC (in USD) = '
     symbols = exchange_rates['USD'].keys()
     symbols.sort()
+
     for each in symbols:
         if each.replace('USD', '') in exchanges:
             response += '%s: %s | ' % (each, exchange_rates['USD'][each])
-    response += 'lolcat (coinbase) index: $%s | ' % (ppnum(float(
-        exchange_rates['USD']['coinbase']) * 160))
-    response += 'Howells (coinbase) index: $%s | ' % (ppnum(float(
-        exchange_rates['USD']['coinbase']) * 7500))
+
+    response += 'lolcat (coinbase) index: $%s | ' % (ppnum(float(exchange_rates['USD']['coinbase']) * 160))
+
+    response += 'Howells (coinbase) index: $%s | ' % (ppnum(float(exchange_rates['USD']['coinbase']) * 7500))
+
     response += 'last updated at: %s UTC' % (str(last_check))
+
     jenni.say(response)
 btc.commands = ['btc']
 btc.example = '.btc'
-btc.rate = 5
+btc.rate = 20
 
 
 def fbtc(jenni, input):
+    '''.fbtc - returns prices from "The Fucking Bitcoin"'''
     try:
-        page = web.get('http://thefuckingbitcoin.com/')
+        page = proxy.get('http://thefuckingbitcoin.com/')
     except:
         return jenni.say('Could not access thefuckingbitcoin.com')
+
     price = re.search('<p id="lastPrice">(\S+)</p>', page)
-    remarks = re.search('<p id="remark">(.*?)</p><p id="remarkL2">(.*?)</p>',
-                        page)
+    remarks = re.search('<p id="remark">(.*?)</p><p id="remarkL2">(.*?)</p>', page)
+
     try:
         remarks = remarks.groups()
     except:
         return jenni.say('Could not find relevant information.')
+
     resp = str()
     resp += '1 BTC == %s USD. ' % price.groups()
+
     if remarks:
         resp += '%s %s' % (remarks[0], remarks[1])
+
     jenni.say(resp)
+
 fbtc.commands = ['fbtc']
 fbtc.example = '.fbtc'
+fbtc.rate = 20
 
 
 if __name__ == '__main__':
