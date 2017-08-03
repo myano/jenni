4f4128a9c1ec11e566d158d2fcf4d96f960f5c40
Michael Yanovich <michael@yanovich.net>
2017-08-03 16:25:45 +0000

    added Bitcoin Cash and improved the output

--- modules/btc.py
+++ modules/btc.py
@@ -16,9 +16,12 @@ import datetime as dt
 import json
 import re
 
-exchange_rates = dict()
-last_check = dt.datetime.now()
-exchanges = ['btce', 'rock', 'bitstamp', 'coinbase']
+btc_exchange_rates = dict()
+bcc_exchange_rates = dict()
+last_check_btc = dt.datetime.now()
+last_check_bcc = dt.datetime.now()
+
+btc_exchanges = ['btce', 'rock', 'bitstamp', 'coinbase']
 
 
 def btc_page():
@@ -45,12 +48,12 @@ def ppnum(num):
 
 def btc(jenni, input):
     '''.btc -- display the current prices for Bitcoins'''
-    global exchange_rates
-    global last_check
+    global btc_exchange_rates
+    global last_check_btc
 
     now = dt.datetime.now()
 
-    if (not exchange_rates) or (now - last_check > dt.timedelta(minutes=15)):
+    if (not btc_exchange_rates) or (now - last_check_btc > dt.timedelta(minutes=15)):
         status, page = btc_page()
         json_page = dict()
 
@@ -63,15 +66,15 @@ def btc(jenni, input):
         else:
             return jenni.reply(page)
 
-        if 'USD' not in exchange_rates:
-            exchange_rates['USD'] = dict()
+        if 'USD' not in btc_exchange_rates:
+            btc_exchange_rates['USD'] = dict()
         ## build internal state of exchange
         for each in json_page:
             if each['currency'] == 'USD':
-                if 'USD' not in exchange_rates:
-                    exchange_rates['USD'] = dict()
-                exchange_rates['USD'][each['symbol'].replace('USD', '')] = each['close']
-        last_check = dt.datetime.now()
+                if 'USD' not in btc_exchange_rates:
+                    btc_exchange_rates['USD'] = dict()
+                btc_exchange_rates['USD'][each['symbol'].replace('USD', '')] = each['close']
+        last_check_btc = dt.datetime.now()
 
         coinbase_status, coinbase_page = btc_coinbase_page()
 
@@ -84,21 +87,27 @@ def btc(jenni, input):
             pass
 
         if coinbase_json:
-            exchange_rates['USD']['coinbase'] = ppnum(float(coinbase_json['btc_to_usd']))
+            btc_exchange_rates['USD']['coinbase'] = ppnum(float(coinbase_json['btc_to_usd']))
 
-    response = '1 BTC (in USD) = '
-    symbols = exchange_rates['USD'].keys()
+    response = '\x02\x0304One (1) Bitcoin (BTC) in USD\x03:\x02 '
+    symbols = btc_exchange_rates['USD'].keys()
     symbols.sort()
 
     for each in symbols:
-        if each.replace('USD', '') in exchanges:
-            response += '%s: %s | ' % (each, ppnum(float(str(exchange_rates['USD'][each]).replace(',', ''))))
+        if each.replace('USD', '') in btc_exchanges:
+            fix_var = str(btc_exchange_rates['USD'][each]).replace(",", '')
+            fix_var = float(fix_var)
+            fix_var = ppnum(fix_var)
+            response += '\x1F{0}\x1F: ${1} | '.format(each.title(), fix_var)
 
-    response += 'lolcat (coinbase) index: $%s | ' % (ppnum(float(str(exchange_rates['USD']['coinbase']).replace(',', '')) * 160))
+    coinbase_rate = str(btc_exchange_rates['USD']['coinbase']).replace(',', '')
+    coinbase_rate = float(coinbase_rate)
 
-    response += 'Howells (coinbase) index: $%s | ' % (ppnum(float(str(exchange_rates['USD']['coinbase']).replace(',', '')) * 7500))
+    response += '\x1Flolcat\x1F (coinbase) index: ${0} | '.format(ppnum(160.0 * coinbase_rate))
+    response += '\x1FHowells\x1F (coinbase) index: ${0} | '.format(ppnum(7500.0 * coinbase_rate))
 
-    response += 'last updated at: %s UTC' % (str(last_check))
+    temp_time = last_check_btc.strftime('%Y-%m-%d %H:%M')
+    response += '\x02Last updated\x02 at: {0} UTC'.format(temp_time)
 
     jenni.say(response)
 btc.commands = ['btc']
@@ -132,6 +141,61 @@ def fbtc(jenni, input):
 fbtc.commands = ['fbtc']
 fbtc.example = '.fbtc'
 fbtc.rate = 20
+
+
+def bitfinex_retrieve(page):
+    bitfinex_json = json.loads(page)
+    return bitfinex_json[-4]
+
+
+def bcc(jenni, input):
+    '''.bcc -- display the current prices for Bitcoin Cash'''
+    global last_check_bcc
+    global bcc_exchange_rates
+
+    now = dt.datetime.now()
+    response = '\x02\x0303One (1) Bitcoin Cash (BCH) in USD:\x03\x02 '
+
+    bcc_exchanges = {
+            'Bitfinex': ['https://api.bitfinex.com/v2/ticker/tBCHUSD', lambda x: json.loads(x)[-4]],
+            'Bittrex': ['https://bittrex.com/api/v1.1/public/getticker?market=USDT-BCC', lambda x: json.loads(x)['result']['Last']],
+            'CoinMarketCap': ['https://api.coinmarketcap.com/v1/ticker/bitcoin-cash/', lambda x: json.loads(x)[0]['price_usd']],
+        }
+
+    exchanges_status = list()
+
+    if (not bcc_exchange_rates) or (now - last_check_bcc > dt.timedelta(minutes=15)):
+
+        for bcc_exchange in bcc_exchanges:
+            try:
+                bcc_page = web.get(bcc_exchanges[bcc_exchange][0])
+            except:
+                exchanges_status.append(False)
+                continue
+
+            try:
+                exchange_price = bcc_exchanges[bcc_exchange][1](bcc_page)
+            except:
+                exchanges_status.append(False)
+                continue
+
+            exchanges_status.append(True)
+
+            bcc_exchange_rates[bcc_exchange] = exchange_price
+            last_check_bcc = dt.datetime.now()
+
+        if not any(exchanges_status):
+            return jenni.say('We could not access any of the APIs.')
+
+    for exchange in bcc_exchange_rates:
+        response += '\x1F{0}\x1F: ${1:.2f}, '.format(exchange, round(float(bcc_exchange_rates[exchange]), 2))
+
+    temp_time = last_check_bcc.strftime('%Y-%m-%d %H:%M')
+    response += '\x02Last updated\x02 at: {0} UTC'.format(temp_time)
+
+    #print "exchanges_status:", exchanges_status
+    jenni.say(response)
+bcc.commands = ['bcc']
 
 
 if __name__ == '__main__':
