796889854c5449ff837fc8c5729578b307657fdc
Kim <kimspindel@gmail.com>
2017-10-29 20:05:01 +0100

    Change IRCname to reflect owner

--- configs.py
+++ configs.py
@@ -29,7 +29,7 @@ class Configs():
                 module.prefix = r'\.'
 
             if not hasattr(module, 'name'):
-                module.name = 'jenni yanosbot, https://git.io/jenni'
+                module.name = 'owner: kimchi'
 
             if not hasattr(module, 'port'):
                 module.port = 6667
